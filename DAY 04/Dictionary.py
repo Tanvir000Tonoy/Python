6f944d2e26d834d41649af948da4e06255203631
developertonoy <happy2024.tat@gmail.com>
2024-08-13 21:06:09 +0600

    Dictionary is roughly covered please practice if possible I will take intuitives in the first run so avoiding conflicts with mind

---
+++ DAY 04/Dictionary.py
@@ -0,0 +1,12 @@
+myInfo ={
+    "Name": "Tanvir",
+    "age":  20,
+    "phone": "01306812870"
+}
+print(myInfo)
+print(type(myInfo))
+print(myInfo["Name"])
+print(len(myInfo))
+newDic = dict(Name="ador", Age="19", Sex= "Female")
+print(newDic)
+print(type(newDic))
